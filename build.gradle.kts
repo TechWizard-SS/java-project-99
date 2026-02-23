213dea53509397e1556296847fe3013dca19348c
TechWizard-SS <samir898877.s@gmail.com>
2026-02-24 00:32:08 +0400

    Проверка на покрытие

--- build.gradle.kts
+++ build.gradle.kts
@@ -106,17 +106,17 @@ tasks.jacocoTestReport {
 	}
 
 	// Классы, которые не требуют покрытия тестами
-	classDirectories.setFrom(
-		sourceSets.main.get().output.asFileTree.matching {
-			exclude(
-				"hexlet/code/util/**",
-				"hexlet/code/component/**",
-				"hexlet/code/AppApplication.class",
-				"hexlet/code/model/**",
-				"hexlet/code/model/dto/**"
-			)
-		}
-	)
+//	classDirectories.setFrom(
+//		sourceSets.main.get().output.asFileTree.matching {
+//			exclude(
+//				"hexlet/code/util/**",
+//				"hexlet/code/component/**",
+//				"hexlet/code/AppApplication.class",
+//				"hexlet/code/model/**",
+//				"hexlet/code/model/dto/**"
+//			)
+//		}
+//	)
 }
 
 tasks.withType<Test> {
